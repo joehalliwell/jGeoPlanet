c8628a1fb7bfc1436ddb66b9d836c87e1dbcf753
Joe Halliwell <joe@winterwell.com>
2009-12-06 23:57:48 +0000

    Bah. Fix comments.

--- src/main/java/winterwell/jgeoplanet/Location.java
+++ src/main/java/winterwell/jgeoplanet/Location.java
@@ -18,17 +18,18 @@ public class Location {
 		this.longitude = jsonObject.getDouble("longitude");
 	}
 	
+
 	/**
-	 * Returns the latitude of this location.
-	 * @return the latitude of this location.
+	 * Returns  the longitude of this location.
+	 * @return the longitude of this location.
 	 */
 	public double getLongitude() {
 		return longitude;
 	}
 	
 	/**
-	 * Returns  the longitude of this location.
-	 * @return the longitude of this location.
+	 * Returns the latitude of this location.
+	 * @return the latitude of this location.
 	 */
 	public double getLatitude() {
 		return latitiude;

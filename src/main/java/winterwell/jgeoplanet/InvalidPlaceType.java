3ef1c1c4b71be9621d0cecffdb4893a14a66db84
Joe Halliwell <joe@winterwell.com>
2009-12-07 00:25:13 +0000

    Add forgotten file.

---
+++ src/main/java/winterwell/jgeoplanet/InvalidPlaceType.java
@@ -0,0 +1,27 @@
+package winterwell.jgeoplanet;
+
+/**
+ * Thrown if an invalid place type is cited.
+ * 
+ * @author Joe Halliwell <joe@winterwell.com>
+ */
+public class InvalidPlaceType extends GeoPlanetException {
+
+	private static final long serialVersionUID = -4976221039931412361L;
+	private String placeTypeName;
+	
+	public InvalidPlaceType(String placeTypeName) {
+		super("Invalid place type: " + placeTypeName);
+		this.placeTypeName = placeTypeName;
+	}
+	
+	/**
+	 * Returns the invalid place type name that caused this
+	 * exception.
+	 * @return the invalid place type name
+	 */
+	public String getInvalidPlaceTypeName() {
+		return placeTypeName;
+	}
+
+}

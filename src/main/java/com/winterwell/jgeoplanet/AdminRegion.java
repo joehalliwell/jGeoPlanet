e66762c638d7a85841e1ff18b45804f021ef51b3
Joe Halliwell <joe@winterwell.com>
2010-02-09 19:12:26 +0000

    Equip location and adminregion with equals() and hashcode()

--- src/main/java/com/winterwell/jgeoplanet/AdminRegion.java
+++ src/main/java/com/winterwell/jgeoplanet/AdminRegion.java
@@ -6,15 +6,15 @@ import org.json.JSONObject;
 /**
  * An administrative region such as: State, Country, County, Province, District, Ward.
  * These are not the same as place types.
- * 
+ *
  * @author Joe Halliwell <joe@winterwell.com>
  */
 public class AdminRegion extends GeoPlanetResource {
-	
+
 	private final String name;
 	private final String type;
 	private final String code;
-	
+
 	AdminRegion(GeoPlanet client, JSONObject place, String name) throws JSONException {
 		super(client);
 		this.name = place.getString(name);
@@ -24,7 +24,7 @@ public class AdminRegion extends GeoPlanetResource {
 		this.code = (code.equals("") ? null : code);
 		this.type = attrs.getString("type");
 	}
-	
+
 	/**
 	 * Returns the name of this administrative region.
 	 * @return the name of this administrative region.
@@ -53,8 +53,53 @@ public class AdminRegion extends GeoPlanetResource {
 
 	@Override
 	public String toString() {
-		return "AdminRegion [code=" + code + ", name=" + name + ", type="
-				+ type + "]";
+		return "AdminRegion [code=" + code + ", name=" + name + ", type=" + type + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((code == null) ? 0 : code.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((type == null) ? 0 : type.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		AdminRegion other = (AdminRegion) obj;
+		if (code == null) {
+			if (other.code != null) {
+				return false;
+			}
+		} else if (!code.equals(other.code)) {
+			return false;
+		}
+		if (name == null) {
+			if (other.name != null) {
+				return false;
+			}
+		} else if (!name.equals(other.name)) {
+			return false;
+		}
+		if (type == null) {
+			if (other.type != null) {
+				return false;
+			}
+		} else if (!type.equals(other.type)) {
+			return false;
+		}
+		return true;
 	}
 
 }
--- src/main/java/com/winterwell/jgeoplanet/Location.java
+++ src/main/java/com/winterwell/jgeoplanet/Location.java
@@ -5,19 +5,19 @@ import org.json.JSONObject;
 
 /**
  * A geographical location expressed as a latitude and longitude
- * 
+ *
  * @author Joe Halliwell <joe@winterwell.com>
  */
 public class Location {
-	
+
 	private final double longitude;
 	private final double latitiude;
-	
+
 	Location(JSONObject jsonObject) throws JSONException {
 		this.latitiude = jsonObject.getDouble("latitude");
 		this.longitude = jsonObject.getDouble("longitude");
 	}
-	
+
 
 	/**
 	 * Returns  the longitude of this location.
@@ -26,12 +26,49 @@ public class Location {
 	public double getLongitude() {
 		return longitude;
 	}
-	
+
 	/**
 	 * Returns the latitude of this location.
 	 * @return the latitude of this location.
 	 */
 	public double getLatitude() {
 		return latitiude;
+	}
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		long temp;
+		temp = Double.doubleToLongBits(latitiude);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(longitude);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		Location other = (Location) obj;
+		if (Double.doubleToLongBits(latitiude) != Double
+				.doubleToLongBits(other.latitiude)) {
+			return false;
+		}
+		if (Double.doubleToLongBits(longitude) != Double
+				.doubleToLongBits(other.longitude)) {
+			return false;
+		}
+		return true;
 	}
 }
